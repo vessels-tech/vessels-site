db7878e08db37b426e3d6e93732373a20f2e039a
Lewis Daly <lewis.daly@enabled.com.au>
2018-03-07 22:23:17 +1030

    new site is live! still need to fix a few things

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# Build hugo
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+rm -rf public
+
+cd $DIR/site
+hugo
+cp -R $DIR/site/public $DIR/public
+
+# deploy to firebase
 firebase deploy --only hosting
